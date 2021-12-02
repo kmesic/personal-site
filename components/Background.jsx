03695935a6d525a4b23dbe7ff057f5b491cc724a
Kenan Mesic <kenanmesic24@gmail.com>
2021-12-01 22:43:02 -0800

    Cleaned up background image to be much faster

--- components/Background.jsx
+++ components/Background.jsx
@@ -1,11 +1,16 @@
-import Image from 'next/image'
+// import Image from 'next/image'
 import Particles from './Particles';
 
+const cssBackgroundImage = {
+    background: "linear-gradient( 109.6deg,  rgba(5,84,94,1) 16%, #bbb 91.1% )"  
+};
 export default function Background() {
     return (
         <>
-            <div className="-z-1 inset-0 fixed h-screen w-screen">
-                <Image
+            <div className="-z-1 inset-0 fixed h-screen w-screen opacity-50" 
+                 style={cssBackgroundImage}
+            >
+                {/* <Image
                 className="h-screen w-screen overflow-hidden"
                 src="/background.jpeg"
                 alt="Mountains"
@@ -14,7 +19,7 @@ export default function Background() {
                 quality={30}
                 priority
                 />
-                <div className="inset-0 fixed h-screen w-screen overflow-hidden bg-black	opacity-50"></div>
+                <div className="inset-0 fixed h-screen w-screen overflow-hidden bg-black	opacity-50"></div> */}
             </div>
             <Particles />
         </>
